f16ac44ca20a8b0ccc7a8b75dd2555c6beb343eb
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 12:54:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -40,7 +40,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   JOIN pledges
   ON pledges.users_id = users.id
   GROUP BY users.name 
-  ORDER BY summed_amount ASC;
+  ORDER BY summed_amount;
   "
 end
 
