a750d9d83742eb763c48e63045da99d3dc830d89
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 12:37:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,6 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT project.titles, (projects.funding_goal - SUM(pledges.amount))* - 1 AS over_goal
   FROM projects
   
+  
   "
 end
 
