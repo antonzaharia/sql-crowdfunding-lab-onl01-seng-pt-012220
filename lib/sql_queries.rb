c842b1510fd8a5f83c69469768bb022c7c078957
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 12:55:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, SUM(pledges.amount) AS summed_amount
   FROM users
   INNER JOIN pledges
-  ON pledges.users_id = users.id
+  ON pledges.user_id = users.id
   GROUP BY users.name 
   ORDER BY summed_amount;
   "
