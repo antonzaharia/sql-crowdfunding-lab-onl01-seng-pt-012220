8d057adf354ed0fc55fb6bd086d01243a67589c4
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 13:05:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,7 +49,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  WHERE project.category = 'music';
+  WHERE category = 'music';
   
   "
 end
