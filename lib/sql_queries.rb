5e02a12027fa867b1c5b6a205412200901f3217a
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 12:29:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,8 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM users
   JOIN pledges
-  ON pledges.user_id = 
+  ON pledges.user_id = users.id
+  GROUP BY users.name;
   "
 end
 
